5f9c5aba98dad32068541507b8aa9fc4bebc37cf
TNiedziela <63972714+TNiedziela@users.noreply.github.com>
2020-04-25 17:05:40 +0200

    Add files via upload
    Żeby otworzyć najpierw pobrać cały folder, potem otworzyć go jako projekt w pycharm i odpalić main. Jeśli coś nie będzie działać to prawdopodobnie trzeba zainstalować pipenv i pygame

---
+++ Interface_24.04.2020/mklawiatury_config.py
@@ -0,0 +1,14 @@
+class player:
+    def __init__(self, nick):
+        self.nick = nick
+        self.pl_scores = []
+
+    def add_score(self, new_score):
+        self.pl_scores.extend(new_score)
+    
+
+
+
+
+
+
