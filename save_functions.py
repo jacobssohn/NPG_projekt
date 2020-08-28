719cfb5f2d6d21fd156b4d137b8c698e18ca9cb1
KamilKaliwoda <64084807+KamilKaliwoda@users.noreply.github.com>
2020-08-28 16:08:16 +0200

    Update save_functions.py
    Teraz skrypt jest już praktycznie gotowy. Wystarczy go tylko zsynchronizować z całością projektu.

--- save_functions.py
+++ save_functions.py
@@ -1,11 +1,12 @@
-def Add_Result(Player_Name, Time, Difficulty): #funkcja jako argumenty przyjmuje nick gracza, czas w sekundach oraz
+def Add_Result(Player_Name:str, Time:float, Difficulty:str): #funkcja jako argumenty przyjmuje nick gracza, czas w sekundach oraz
     Player_File = str(Player_Name) + ".txt"    #poziom trudnosci na ktorym byla rozgrywana gra
     string = str(Difficulty) + " " + str(float(Time)) + "\n"
     open(Player_File,"a+").write(string)
     if len(open(Player_File,"r").readlines())>1:
-        Sorting(Player_File)
+        Sorting(Player_File)                   #funkcja Sorting wywolywana jest automatycznie po dodaniu nowego wyniku do
+                                               #pliku tekstowego
 
-def Sorting(Player_File): #Funkcja sortujaca statystyki w pliku tekstowym
+def Sorting(Player_File:str):                      #Funkcja sortujaca statystyki w pliku tekstowym
     file = open(Player_File, "r+").readlines()
     open(Player_File,"w").close()
     if file != None:
@@ -15,23 +16,24 @@ def Sorting(Player_File): #Funkcja sortujaca statystyki w pliku tekstowym
             tuple=(list[0],float(list[1]))
             result_tuples.append(tuple)
         sorted_results = sorted(result_tuples, key= lambda tuple: tuple[1])
-        sorted_results.reverse()  #Zakomentowanie tej linii zamieni kolejnosc wypisywania
+        #sorted_results.reverse()  #Odkomentowanie tej linii zamieni kolejnosc wypisywania
         sorted_list=[]
         for results in sorted_results:
             line = results[0] + " " + str(results[1]) +"\n"
             sorted_list.append(line)
         file = open(Player_File,"r+").writelines(sorted_list)
 
-def Get_Results(Player_Name, Difficulty):      #funkcja przyjmuje jako argumenty nick gracza, oraz poziom trudnosci, ktory ma zostac
+def Get_Results(Player_Name:str, Difficulty:str):      #funkcja przyjmuje jako argumenty nick gracza, oraz poziom trudnosci, ktory ma zostac
     Player_File = str(Player_Name) + ".txt"    #odczytany (oba typu string) i zwraca tablice 5 najwyzszych wynikow uzyskanych na
-    file = open(Player_File,"r+").readlines()  #danym poziomie trudnosci
-    result = []
+    file = open(Player_File,"r+").readlines()  #danym poziomie trudnosci zaczynajac od najlepszego czasu
+    Result = []
     for words in file:
         line = words.split()
         array = [str(line[0]),float(line[1])]
         if array[0] == Difficulty:
-            result.append(array[1])
-    if len(result) > 5:
-        result = result[0:5]
-    return result
+            Result.append(array[1])
+    if len(Result) > 5:
+        Result = Result[0:5]
+    return Result
+
 
